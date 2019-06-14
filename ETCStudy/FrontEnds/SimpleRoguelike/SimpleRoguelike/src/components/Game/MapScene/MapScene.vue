d0c586e0fe39bb92c692b1d080685bd5288fdb2d
ChoHangMin <chm0326@contela.com>
2019-06-14 18:00:26 +0900

    0614

--- ETCStudy/FrontEnds/SimpleRoguelike/SimpleRoguelike/src/components/Game/MapScene/MapScene.vue
+++ ETCStudy/FrontEnds/SimpleRoguelike/SimpleRoguelike/src/components/Game/MapScene/MapScene.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="map_scene">
+    <canvas id="canvas" width="300" height="300"></canvas>
   </div>
 </template>
 
@@ -23,5 +24,13 @@ export default {
 #map_scene {
   height: 100%;
   background-color : #524d69;
+  display: -webkit-flex; /* Safari */
+  -webkit-flex-flow: row wrap; /* Safari 6.1+ */
+  display: flex;
+  flex-flow: row wrap;
+  #canvas{
+    width: 300px;
+    height: 300px;
+  }
 }
 </style>
